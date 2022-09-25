fbb34591837013e94ff20e77628ad7674d4b2782
Rhinoceroach <whyamiso24.7@gmail.com>
2022-09-25 15:47:08 -0400

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Fade, Slide } from "react-reveal";
+
+class Contact extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const email = this.props.data.email;
+    const message = this.props.data.contactmessage;
+
+    return (
+      <section id="contact">
+        <Fade bottom duration={1000}>
+          <div className="row section-head">
+            <div className="two columns header-col">
+              <h1>
+                <span>Get In Touch.</span>
+              </h1>
+            </div>
+
+            <div className="ten columns">
+              <p className="lead">{message}</p>
+              <a href='mailto:j-cai@outlook.com'>{email}</a>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Contact;
